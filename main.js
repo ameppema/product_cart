90f28f9694012903f5307cf537e0f742ce8c312a
ameppema <35190763+ameppema@users.noreply.github.com>
2024-11-04 08:59:43 +0100

    Building main card elements

--- main.js
+++ main.js
@@ -1,12 +1,58 @@
+
 fetch("http://127.0.0.1:5500/data.json", { method: "GET" })
   .then((response) => response.json())
   .then((result) => {
-		const products = document.querySelector('.products')
+    const productsContainer = document.querySelector('.products');
+    
+    result.forEach(item => {
+      const product = document.createElement('div');
+      product.classList.add('product', 'p-8', 'inline-block');
 
-		result.forEach(item => {
-			products.innerHTML = item.name
-		});
+      const header = document.createElement('div');
+      header.classList.add('product-header', 'relative', 'flex-wrap');
+      product.append(header);
+      
+      const image = document.createElement('img');
+			image.classList.add('rounded-2xl');
+      image.src = item.image.mobile;
+      image.alt = item.name;
+      header.append(image);
+      
+      const button = document.createElement('button');
+      button.classList.add('flex', 'absolute','top-1/2', 'right-1/2','translate-x-2/4', 'rounded-[2rem]', 'p-4', 'border-2', 'border-[#a8a8a8]');
 
-		console.log(result)
-	})
-  .catch((error) => console.error(error));
+      const icon = document.createElement('img');
+      icon.src = '/assets/images/icon-add-to-cart.svg';
+      icon.alt = 'Add to Cart Icon'; 
+
+      const label = document.createElement('span');
+      label.innerText = 'Add to Cart';
+      
+      button.append(icon, label);
+      header.append(button);
+
+      const body = document.createElement('div');
+      body.classList.add('product-body', 'flex', 'flex-col');
+			
+      const category = document.createElement('span');
+      category.classList.add('product-category'); 
+      category.innerText = item.category;
+
+      const name = document.createElement('span');
+      name.classList.add('product-name'); 
+      name.innerText = item.name;
+
+      const price = document.createElement('span');
+      price.classList.add('product-price');
+      price.innerText = item.price;
+
+			body.append(category, name, price);
+
+      product.append(body);
+
+      productsContainer.append(product);
+    });
+
+    console.log(result);
+  })
+  .catch((error) => console.error('Error fetching data:', error));
